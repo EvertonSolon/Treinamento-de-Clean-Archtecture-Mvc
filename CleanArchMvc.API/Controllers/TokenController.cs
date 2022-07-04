47e9600898ac28e203926cd4ee56c3cad7385637
Everton Solon Tiba <evertonsolon@outlook.com>
2022-07-04 13:41:16 -0300

    Criação do TokenController

---
+++ CleanArchMvc.API/Controllers/TokenController.cs
@@ -0,0 +1,36 @@
+﻿using CleanArchMvc.API.Models;
+using CleanArchMvc.Domain.Accounts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IAuthenticate _authenticate;
+        public TokenController(IAuthenticate authenticate)
+        {
+            _authenticate = authenticate ?? 
+                throw new ArgumentNullException(nameof(authenticate));
+        }
+
+        [HttpPost("LoginUser")]
+        public async Task<ActionResult<UserToken>> Login([FromBody] LoginModel userInfo)
+        {
+            var result = await _authenticate.AuthenticateAsync(userInfo.Email, userInfo.Password);
+
+            if (result)
+            {
+                //TODO
+                //return GenerateToken(userInfo);
+
+                return Ok($"User {userInfo.Email} login successfully!");
+            }
+
+            ModelState.TryAddModelError(string.Empty, "Invalid Login attempt.");
+            return BadRequest(ModelState);
+        }
+    }
+}
